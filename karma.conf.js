8dfad6f7e0d11c6d6e74e404738b0787092b2600
Ben Baik <benbaik@Bens-MacBook-Pro.local>
2017-03-23 09:36:10 -0600

    initial commit

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine', 'browserify'],
+        files: [
+            'src/**/*.js',
+            'test/**/*_spec.js'
+        ],
+        preprocessors: {
+            'src/**/*.js': ['jshint', 'browserify'],
+            'test/**/*.js': ['jshint', 'browserify']
+        },
+        browsers: ['PhantomJS'],
+        browserify: {
+            debug: true
+        }
+    });
+}
