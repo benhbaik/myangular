3fa84f343dc610b207ce62666ed1d2c3f5b12d85
Ben Baik <benbaik@Bens-MacBook-Pro.local>
2017-03-23 18:00:14 -0600

    added error handling

--- src/scope.js
+++ src/scope.js
@@ -9,32 +9,44 @@ function Scope() {
 function initWatchVal() {}
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
+    var self = this;
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function() {},
         valueEq: !!valueEq,
         last: initWatchVal
     };
-    this.$$watchers.push(watcher);
-    this.$$lastDirtyWatch = null;
+    self.$$watchers.push(watcher);
+    self.$$lastDirtyWatch = null;
+    return function() {
+        var index = self.$$watchers.indexOf(watcher);
+        if (index >= 0) {
+            self.$$watchers.splice(index, 1);
+        }
+    };
 };
+
 Scope.prototype.$$digestOnce = function() {
     var self = this;
     var newValue, oldValue, dirty;
     _.forEach(this.$$watchers, function(watcher) {
-        newValue = watcher.watchFn(self);
-        oldValue = watcher.last;
-        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-            self.$$lastDirtyWatch = watcher;
-            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-            watcher.listenerFn(
-                newValue,
-                (oldValue === initWatchVal ? newValue : oldValue),
-                self
-            );
-            dirty = true;
-        } else if (self.$$lastDirtyWatch === watcher) {
-            return false;
+        try {
+            newValue = watcher.watchFn(self);
+            oldValue = watcher.last;
+            if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                self.$$lastDirtyWatch = watcher;
+                watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+                watcher.listenerFn(
+                    newValue,
+                    (oldValue === initWatchVal ? newValue : oldValue),
+                    self
+                );
+                dirty = true;
+            } else if (self.$$lastDirtyWatch === watcher) {
+                return false;
+            }
+        } catch(e) {
+            console.log(e);
         }
     });
     return dirty;
@@ -57,7 +69,9 @@ Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
     if (valueEq) {
         return _.isEqual(newValue, oldValue);
     } else {
-        return newValue === oldValue;
+        return newValue === oldValue ||
+        (typeof newValue === 'number' && typeof oldValue === 'number' &&
+         isNaN(newValue) && isNaN(oldValue));
     }
 };
 
